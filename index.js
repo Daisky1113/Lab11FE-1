47259842f9d789873fd0edf03e2c8252ac6c32d4
r0ckandc0de <yohtaro.s@gmail.com>
2021-07-13 19:21:06 +0900

    1st commit

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('hello');
+
+//DOMの取得
+const input = getElementById('js_input');
+const text = getElementById('js_text');
+const save = getElementById('js_save');
+const clear = getElementById('js_clear');
+
+
+//1.Save クリックイベント
+
+
+
+
+//2.clear クリックイベント
+
+
+
+
+
+//3.ページ読み込み：保存データ取得表示
+
+
